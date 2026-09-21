18475e9d6617da51dfbe5bfaac97353d7c50554e
Patrick Morais <111186316+patrickmf97@users.noreply.github.com>
2026-09-21 14:57:12 -0300

    feat: keep economy simulation running inside buildings

--- src/scenes/InteriorScene.ts
+++ src/scenes/InteriorScene.ts
@@ -17,6 +17,7 @@ import { TimeSystem } from '../systems/TimeSystem';
 import { LifeSimulationSystem } from '../systems/LifeSimulationSystem';
 import { NpcBrainSystem } from '../systems/NpcBrainSystem';
 import { DynamicDialogueSystem } from '../systems/DynamicDialogueSystem';
+import { EconomySystem } from '../systems/EconomySystem';
 import { Hud } from '../ui/Hud';
 
 interface InteriorSceneData {
@@ -42,6 +43,7 @@ export class InteriorScene extends Phaser.Scene {
   private lifeSystem!: LifeSimulationSystem;
   private brainSystem!: NpcBrainSystem;
   private dynamicDialogue!: DynamicDialogueSystem;
+  private economySystem!: EconomySystem;
   private hud!: Hud;
 
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
@@ -50,6 +52,7 @@ export class InteriorScene extends Phaser.Scene {
   private enterKey!: Phaser.Input.Keyboard.Key;
   private brainKey!: Phaser.Input.Keyboard.Key;
   private freeChatKey!: Phaser.Input.Keyboard.Key;
+  private marketKey!: Phaser.Input.Keyboard.Key;
   private persistAccumulator = 0;
 
   constructor() {
@@ -90,6 +93,7 @@ export class InteriorScene extends Phaser.Scene {
 
     this.brainSystem = new NpcBrainSystem(this.save, this.lifeSystem);
     this.dynamicDialogue = new DynamicDialogueSystem(this.save);
+    this.economySystem = new EconomySystem(this.save, this.lifeSystem);
     this.dialogue = new DialogueSystem();
     this.hud = new Hud();
 
@@ -115,6 +119,7 @@ export class InteriorScene extends Phaser.Scene {
     this.enterKey = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     this.brainKey = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.B);
     this.freeChatKey = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    this.marketKey = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.M);
 
     this.cameras.main.setBounds(0, 0, INTERIOR_SIZE.width, INTERIOR_SIZE.height);
     this.cameras.main.centerOn(INTERIOR_SIZE.width / 2, INTERIOR_SIZE.height / 2);
@@ -167,6 +172,28 @@ export class InteriorScene extends Phaser.Scene {
       this.timeSystem.minuteOfDay,
       simulationDt,
     );
+
+    const economyEvents = this.economySystem.update(
+      this.timeSystem.day,
+      this.timeSystem.minuteOfDay,
+      simulationDt,
+    );
+    const importantEconomyEvent = economyEvents
+      .slice()
+      .reverse()
+      .find((event) =>
+        [
+          'shortage',
+          'construction-start',
+          'construction-complete',
+        ].includes(event.type),
+      );
+    if (importantEconomyEvent) {
+      this.hud.showToast(
+        '📊 ' + importantEconomyEvent.text,
+      );
+    }
+
     this.syncResidentRoster();
     this.syncResidents();
 
@@ -204,6 +231,14 @@ export class InteriorScene extends Phaser.Scene {
 
     if (!this.dynamicDialogue.isOpen && Phaser.Input.Keyboard.JustDown(this.brainKey)) {
       this.hud.toggleBrainDebug();
+    }
+
+    if (
+      !this.dialogue.isOpen &&
+      !this.dynamicDialogue.isOpen &&
+      Phaser.Input.Keyboard.JustDown(this.marketKey)
+    ) {
+      this.hud.toggleEconomyDebug();
     }
 
     this.persistAccumulator += dt;
@@ -449,6 +484,9 @@ export class InteriorScene extends Phaser.Scene {
       return name + ' → ' + log.label + ' [' + log.score + '] • ' + reason;
     });
     this.hud.setBrainDebug(brainLines.length ? brainLines : ['Aguardando decisões...']);
+    this.hud.setEconomyDebug(
+      this.economySystem.getSummaryLines(),
+    );
     this.hud.setPopulation(this.lifeSystem.getAllDefinitions().length);
     this.hud.setClock(
       this.timeSystem.formatted,
